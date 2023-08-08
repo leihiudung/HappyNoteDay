325b0b53e94f3892aa68afc9303db448d6493693
LiHiudong <245822210@qq.com>
2023-08-08 10:41:17 +0800

    build project

---
+++ src/main/java/com/example/happynoteday/HappyNoteDayApplication.java
@@ -0,0 +1,13 @@
+package com.example.happynoteday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyNoteDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyNoteDayApplication.class, args);
+    }
+
+}
