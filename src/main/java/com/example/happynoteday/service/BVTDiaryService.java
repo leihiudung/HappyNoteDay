b9b080dc85220f4356b6e90f324b4a8125824fee
LiHiudong <245822210@qq.com>
2023-08-08 23:43:21 +0800

    add BVTDiaryServiceImpl file

---
+++ src/main/java/com/example/happynoteday/service/BVTDiaryService.java
@@ -0,0 +1,6 @@
+package com.example.happynoteday.service;
+
+public interface BVTDiaryService {
+
+
+}
---
+++ src/main/java/com/example/happynoteday/service/impl/BVTDiaryServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.happynoteday.service.impl;
+
+import com.example.happynoteday.service.BVTDiaryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BVTDiaryServiceImpl implements BVTDiaryService {
+
+}
